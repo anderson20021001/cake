cfdb3003f4fa79cc9039e8dee53f895dbf727b6a
Werinha <142060514+Werinha@users.noreply.github.com>
2023-09-21 21:38:46 -0300

    vera3

---
+++ resources/views/cake/cobertura/cobertura_show.blade.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <h1>Produto</h1>
+    <strong>Nome:</strong> {{ $produto->nome}}<br>
+    <strong>Quantidade:</strong> {{ $produto->quantidade}}<br>
+    <strong>Preço:</strong> {{ $produto->preco}}<br>
+    <strong>Criação:</strong> {{$produto->create_at}}<br>
+
+</div>
---
+++ resources/views/cake/decoracao/decoracao_create.blade.php
@@ -0,0 +1,43 @@
+@extends('adminlte::page')
+
+@section('content')
+<div>
+    Formulário de Criação:<br>
+
+    @if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+
+
+    <form method="POST" action="{{url('/produto/create')}}">
+        @csrf
+
+        <label for="categoria">Escolha uma categoria:</label>
+
+        <select name="categoria_id" id="categoria">
+             @foreach($categorias as $categoria)
+                <option value="{{ $categoria->id }}">{{ $categoria->nome }}</option>
+            @endforeach
+        </select>
+        <br>
+        
+  <label for="fname">Nome:</label><br>
+  <input type="text" id="fname" name="nome"><br>
+
+  <label for="fname">Quantidade:</label><br>
+  <input type="text" id="fname" name="quantidade"><br>
+
+  <label for="fname">Preço:</label><br>
+  <input type="text" id="fname" name="preco"><br>
+
+  <input type="submit" value="Enviar">
+
+</form>
+
+</div>
