dc0873320e56ab508b8eca2ba8c3b5ce40587596
mariabelizio <cidabelizio@gmail.com>
2023-11-25 21:10:08 -0300

    Update welcome.blade.php
    oiiii

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -27,6 +27,7 @@
     display: inline-flex;
     align-items: center;
   }
+<<<<<<< Updated upstream
 
     .background1 {
       flex-grow: 1; /* Ocupa todo o espaço disponível na div */
@@ -143,6 +144,16 @@ h5{
       </style>
 
 
+=======
+</style>
+
+<h1 style="color:Violet;">Nado Cake's</h1>
+
+<p>
+Reginaldo é um entusiasta da culinária desde da adolescência, e sua mãe foi sua grande inspiração.
+Resolveu investir na ideia e se especializar em fazer bolos para festas, eventos e aprimorou-se em decoração. Criou novos sabores de recheios para atender às preferências dos seus clientes. A marca Nado Cake’s é reconhecida pela qualidade, sabor, criatividade, bolos personalizados e por fazer com que cada celebração seja única e especial. Deixo aqui meu Convite a todos vocês para degustarem verdadeiras obras de arte preparado com muito amor e carinho!!!
+</p>
+>>>>>>> Stashed changes
 
 <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
@@ -372,6 +383,7 @@ h5{
 <br>
     
 
+<<<<<<< Updated upstream
     <div id="whatsapp-icon">
       <a href="javascript:void(0);" onclick="window.open(whatsappURL, '_blank');">
         <img src= "{{ url('/imagens/wpp.png')}}" alt="WhatsApp" width="50px">
@@ -480,6 +492,119 @@ h5{
     </body>
 </html>
     
+=======
+<footer class="bg-dark text-light">
+        <div class="container-fluid py-2">
+            <div class="row">
+                <div class="col-4">
+                    <ul class="nav flex-column">
+                        <li class="nav-link"><a href="#">Retornar à Loja</a></li>
+                        <li class="nav-link"><a href="#">Sobre</a></li>
+                        <li class="nav-link"><a href="#">Contato</a></li>
+                        <li class="nav-link"><a href="#">Suporte</a></li>
+                    </ul>
+                </div>
+                <div class="col-8">
+                    <p>
+                    Quando você sente aquele cheirinho de bolo caseiro e sente saudades do bolo da sua mãe ou avó, logo vem na cabeça "ai que vontade de comer bolo!" Então passa aqui!
+                  </p>
+                    <ul class="nav">
+                        <li class="nav-link"><i class="fab fa-facebook fa-3x"></i></li>
+                        <li class="nav-link"><i class="fab fa-instagram fa-3x"></i></li>
+                        <li class="nav-link"><i class="fab fa-twitter fa-3x"></i></li>
+                        <li class="nav-link"><i class="fab fa-whatsapp fa-3x"></i></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="text-center" style="background-color:rgb(227, 38, 54); padding: 20px;">
+            &copy 2018 Copyright: <a href="#">Lojas Virtuais</a>
+        </div>
+        
+    </footer>
+>>>>>>> Stashed changes
+
+    <!-- <div class="wpp-btn">
+            <a href="https://web.whatsapp.com/send?phone=5521985423502&text= Olá, como podemos ajudar?" target="_blank">
+                <img src="img/wpp-icon.png" alt="">
+            </a>
+        </div> -->
+
+        <span class="wptwa-flag"></span>
+    <svg xmlns="https://www.w3.org/2000/svg" style="display: none;">
+        <symbol id="wptwa-logo">
+            <path id="WhatsApp" d="M90,43.841c0,24.213-19.779,43.841-44.182,43.841c-7.747,0-15.025-1.98-21.357-5.455L0,90l7.975-23.522   c-4.023-6.606-6.34-14.354-6.34-22.637C1.635,19.628,21.416,0,45.818,0C70.223,0,90,19.628,90,43.841z M45.818,6.982   c-20.484,0-37.146,16.535-37.146,36.859c0,8.065,2.629,15.534,7.076,21.61L11.107,79.14l14.275-4.537   c5.865,3.851,12.891,6.097,20.437,6.097c20.481,0,37.146-16.533,37.146-36.857S66.301,6.982,45.818,6.982z M68.129,53.938   c-0.273-0.447-0.994-0.717-2.076-1.254c-1.084-0.537-6.41-3.138-7.4-3.495c-0.993-0.358-1.717-0.538-2.438,0.537   c-0.721,1.076-2.797,3.495-3.43,4.212c-0.632,0.719-1.263,0.809-2.347,0.271c-1.082-0.537-4.571-1.673-8.708-5.333   c-3.219-2.848-5.393-6.364-6.025-7.441c-0.631-1.075-0.066-1.656,0.475-2.191c0.488-0.482,1.084-1.255,1.625-1.882   c0.543-0.628,0.723-1.075,1.082-1.793c0.363-0.717,0.182-1.344-0.09-1.883c-0.27-0.537-2.438-5.825-3.34-7.977   c-0.902-2.15-1.803-1.792-2.436-1.792c-0.631,0-1.354-0.09-2.076-0.09c-0.722,0-1.896,0.269-2.889,1.344   c-0.992,1.076-3.789,3.676-3.789,8.963c0,5.288,3.879,10.397,4.422,11.113c0.541,0.716,7.49,11.92,18.5,16.223   C58.2,65.771,58.2,64.336,60.186,64.156c1.984-0.179,6.406-2.599,7.312-5.107C68.398,56.537,68.398,54.386,68.129,53.938z"/>
+        </symbol>
+    </svg>
+
+    <div class="wptwa-container" data-delay-time="1" data-inactive-time="1" data-scroll-length="50" data-auto-display-on-mobile="off">
+        <div class="wptwa-box">
+            <div class="wptwa-wrapper">              
+                <div class="wptwa-description">
+                    <p>Olá! como podemos ajuda? Clique no atendimento abaixo que em breve responderemos.</p>
+                </div>            
+                <span class="wptwa-close"></span>
+                <div class="wptwa-people">                       
+                    <a href="https://web.whatsapp.com/send?phone=5521985423502&text= Olá, como podemos ajudar?" target="_blank" class="wptwa-account">
+                        <div class="wptwa-face"><img src="img/logo-facebook.jpg" onerror="this.style.display='none'"></div>
+                        <div class="wptwa-info">
+                            <span class="wptwa-title">Atendimento</span>>                        
+                        </div>
+                        <div class="wptwa-clearfix"></div>
+                    </a>                                               
+                </div>
+                             
+                </div>
+            </div>
+        </div>
+        <span class="wptwa-handler">
+            <svg class="WhatsApp" width="15px" height="15px" viewBox="0 0 90 90"><use xlink:href="#wptwa-logo"></svg>
+            <span class="text">Fale conosco via WhatsApp</span>          
+        </span>
+    </div>
+
+        <script src="js/jquery-1.11.2.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script src="js/idangerous.swiper.min.js"></script>
+        <script src="js/isotope.pkgd.min.js"></script>
+        <script src="js/placeholder.min.js"></script>
+        <script src="js/magnific.js"></script>
+        <script src="js/bxslider/jquery.bxslider.min.js"></script>
+        <script src="js/InstagramFeed.js"></script>
+        <script src="js/global.js"></script>
+        <script src="js/wpt-whatsapp.js"></script>
+
+        <div id="fb-root"></div>
+        <script async defer crossorigin="anonymous" src="https://connect.facebook.net/pt_BR/sdk.js#xfbml=1&version=v9.0&appId=2832877280281221&autoLogAppEvents=1" nonce="bK74bsQr"></script>
+        <br/><br/>
+
+<!--CONTATO-->
+<div class="contact f-contact scroll-to-block" >
+    <div class="container">
+        <h2 class="block-title r-block-title">                   
+            Endereço
+        </h2>
+      </div>
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="contact-info">
+  
+                    <p>R. Fernandes Vieira, 666 - Estacio | Rio de Janeiro/RJ</p>
+                    <div class="row">
+                        <div class="col-sm-6">
+                            <h5>Telefone e Email</h5>
+                            <p><a class="phone" href="tel:21985453502">21 9854.535502</a> (tele-entrega) <br> <a class="mail-us" href="mailto:admelianesagaça@gmail.com">admelianesagaça@gmail.com</a></p>
+                        </div>
+                        <div class="col-sm-6">
+                            <h5>Funcionamento</h5>
+                           <p>Segunda - Sábado <br> 09:00 às 19:00</p>
+                        </div>
+                    </div>                      
+                </div>                 
+                
+            </div>
+            
 
 
 @endsection
